02e7a897049e47a6ae99b1d69181854fd3019e25
gness1804 <graham.nessler@gmail.com>
2021-04-18 13:37:31 -0500

    basic fetch and title and description

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
 (async () => {
-  const res = await fetch('https://www.allrecipes.com/recipe/272858/air-fryer-chicken-thighs/');
-  const html = await res.text()
-  console.log('html:', html);
+  let html;
+  try {
+    const res = await fetch('https://www.allrecipes.com/recipe/272858/air-fryer-chicken-thighs/');
+    html = await res.text()
+  } catch (error) {
+    console.error(`Failed to retrieve recipe: ${error}.`);
+    process.exit(1);
+  }
+
+  const $ = cheerio.load(html);
+  const title = $('h1.heading-content').text();
+  const description = $('.recipe-summary p').text();
 })()
